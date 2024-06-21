d3d4801f3f3d3ca66fd10f2b51f8f6d01194a951
david rubal <davidrubal123@gmail.com>
2024-06-21 09:15:56 -0400

    complete blender project

--- app.rb
+++ app.rb
@@ -16,9 +16,78 @@ class App < Sinatra::Base
   end
 
   def title
-    'Davids Blender App'
+    "David's Blender App"
+  end
+  
+  def accounts
+        Process.groups.map do |gid|
+            Etc.getgrgid(gid).name
+        end.select do |group|
+            group.start_with?('P')
+        end
+  end
+  
+  def blend_files
+      Dir.glob("#{__dir__}/blend_files/*.blend")
+  end
+  
+  def images(directory)
+      Dir.glob("#{directory}/*.png")
+  end
+  
+  post '/render/frames' do
+                                                                                          #back-tick can be used to execute terminal commands
+      
+      walltime = format('%02d:00:00', params[:walltime])
+      blend_file = "#{params[:blend_file]}"  
+
+      exports = "BLEND_FILE_PATH=#{params[:blend_file]}"
+      exports = "#{exports},OUTPUT_DIR=#{params[:project_directory]}"
+      exports = "#{exports},FRAME_RANGE=#{params[:frame_range]}"
+      
+      
+      args = ['-A', params[:account], '-n', params[:num_cpus]]
+      args.concat(['-t', walltime, '-M', 'pitzer', '--parsable'])
+      args.concat(['--export', exports])                                                  #single quotes will not be templated at all
+      
+      script = "#{__dir__}/scripts/render_frames.sh"
+      output = `/bin/sbatch #{args.join(' ')} #{script} 2>&1`                             #this last part puts 2 (error stuff stream) to 1("puts" command, print/return)
+    
+      session[:flash] = { info: "Submitted job with id: '#{output.split(';').first}'"}
+                   
+    
+      redirect(url("/projects/#{File.basename(params[:project_directory])}"))  
+     
+  end
+   
+  post '/render/video' do
+    walltime = format('%02d:00:00', params[:walltime])
+    script =  "#{__dir__}/scripts/render_video.sh"
+    exports= "FRAMES_PER_SEC=#{params[:frames_per_second]}"
+    exports= "#{exports},OUTPUT_DIR=#{params[:project_directory]}"
+    args = ['-A', params[:account], '-n', params[:num_cpus], '-M' 'pitzer', '--parsable', '-t', walltime, '--export', exports]
+    
+    output=`/bin/sbatch #{args.join(' ')} #{script} 2>&1`
+    
+    session[:flash] = { info: "Submitting job with id: '#{output}'"}
+    redirect(url("/projects/#{File.basename(params[:project_directory])}"))
+  end
+  
+  
+  def sanitize_project_name(name)
+      name.downcase.gsub(' ', '_')
   end
 
+  def projects_root
+      "#{__dir__}/projects"           #__dir__ is directory of current file
+  end
+  
+  def projects
+      Dir.children("#{projects_root}").select do |path|
+          Pathname.new("#{projects_root}/#{path}").directory?    #returns true if item is a directory
+      end.sort_by(&:to_s)
+  end
+  
   get '/examples' do
     erb(:examples)      #erb = embedded ruby
   end
@@ -29,11 +98,32 @@ class App < Sinatra::Base
     erb(:index)
   end
   
-  get '/projects/new' do
-    erb(:new_project)
+  get '/projects/:name' do
+    if params[:name] == 'new'
+        erb(:new_project)
+    else
+        @directory = Pathname.new("#{projects_root}/#{params[:name]}")
+        @flash = session.delete(:flash)
+        @images = images(@directory)
+        
+        
+        if (@directory.exist? && @directory.directory?)
+            erb(:show_project)
+        else
+            session[:flash] = { danger: "There is no project named '#{params[:name]}'"}
+            redirect(url("/"))
+        end
+    end
   end
   
   post '/projects/new' do
-    redirect(url("/projects/new"))
+  
+    directory_name = sanitize_project_name(params[:name])
+    FileUtils.mkdir_p("#{projects_root}/#{directory_name}")
+    session[:flash] = { info: "Created project '#{params[:name]}'"}
+    redirect(url("/"))
   end
 end
+
+
+
--- public/app.js
+++ public/app.js
@@ -1,4 +1,76 @@
+//.map is used to do stuff with array
 
-jQuery(() => {
-  console.log('hello world');
-});
+window.onload = () => 
+{
+  updateCarousel();
+};
+
+function updateCarousel() {
+    
+    const configElement = document.getElementById('project_config');
+    if(configElement === null)
+    {
+        return; 
+    }
+    
+    const directory = configElement.dataset.directory;
+    console.log(`Looking for images in ${directory}`);
+    
+    const url = `/pun/sys/dashboard/files/fs/${directory}`;
+    const options= {
+        headers: {
+            'Accept': 'application/json'
+                 }
+                   };  //key value pairs in {}
+    
+    fetch(url, options)  //promises to do something in the future, returns a promise
+        .then((response) => {console.log(`fetch resolved at ${new Date().toISOString()}.`);
+            return response;
+        }).then((response) => response.json())
+          .then((json) => json['files'])
+          .then((files) => files.map((file) => file['name']))
+          .then((files) => files.filter((file) => file.endsWith('png')))      //filter is select in ruby
+          .then((files) => files.splice(0,9))
+          .then((files) => {
+              
+              for(const file of files)
+              {
+                  
+                  const image = document.getElementById(file);
+                  
+                  if(image !== null) //triple equals is strict equality, must be exactly the same thing, not just mathmatically equal
+                  {
+                      console.log(`skipping ${file} because its already on the page`);
+                      continue;
+                  }
+                  
+                  const imageUrl = `/pun/sys/dashboard/files/fs/${directory}/${file}`;
+                  console.log(`Adding ${file} to the DOM`);   //Document object model
+                  const newImage = document.createElement('div');
+                  newImage.classList.add('carousel-item');
+                  newImage.innerHTML = `<img class ="d-block w-100" src="${imageUrl}">`;
+                  newImage.id = file;
+                  
+                  const parent = document.getElementById('image_carousel_inner');
+                  parent.append(newImage);
+                  
+                  const indicators = document.getElementById('image_carousel_indicators');
+                  const slideTo = indicators.children.length;
+                  
+                  const newIndicator = document.createElement('li');
+                  newIndicator.setAttribute('data-target', '#image_carousel');
+                  newIndicator.setAttribute('data-slide-to', slideTo);
+                  
+                  indicators.append(newIndicator);
+              }
+              
+              
+          });
+          
+          
+    // "#{variable}"    
+    // is the now "${variable}"
+    
+    console.log(`fetch called at ${new Date().toISOString()}.`);
+    setTimeout(updateCarousel, 10000); //do this thing later (miliseconds)
+}
